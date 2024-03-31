b8c0fa6a07999995a29c5b722366909971999642
wkembel1563 <wkembel@hotmail.com>
2024-03-30 19:26:52 -0500

    Project7 Complete

---
+++ Project7/Petition.swift
@@ -0,0 +1,8 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Will Kembel on 3/27/24.
+//
+
+import Foundation
---
+++ Project7/Petitions.swift
@@ -0,0 +1,8 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Will Kembel on 3/27/24.
+//
+
+import Foundation
