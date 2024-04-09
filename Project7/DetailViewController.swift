32be56a037c19a64aa69c8693177b27660510d23
wkembel1563 <wkembel@hotmail.com>
2024-04-08 20:58:00 -0500

    Project 7 Complete

--- Project7/DetailViewController.swift
+++ Project7/DetailViewController.swift
@@ -6,24 +6,33 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var petition: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let petition = petition else { return }
+        
+        let htmlString = """
+        <html>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        <body>
+        \(petition.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(htmlString, baseURL: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var petitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // load up petition data
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(data)
+            }
+        }
+        else {
+            notifyWithError(msg: "Unable to load URL")
+        }
+    }
+    
+    func parse(_ json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+        else {
+            notifyWithError(msg: "Unable to parse JSON data")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let dc = DetailViewController()
+        dc.petition = petitions[indexPath.row]
+        navigationController?.pushViewController(dc, animated: true)
     }
 
+    func notifyWithError(msg: String) {
+        let ac = UIAlertController(title: "Error", message: msg, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Okay", style: .default))
+        
+        present(ac, animated: true)
+    }
 
 }
 
